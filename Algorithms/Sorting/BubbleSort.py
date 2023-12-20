99b0c42f021c5c1e869c9be798b165a9f2fc714b
AsifAliSoftage <153803870+AsifAliSoftage@users.noreply.github.com>
2023-12-20 11:33:36 +0530

    Update BubbleSort.py

--- Algorithms/Sorting/BubbleSort.py
+++ Algorithms/Sorting/BubbleSort.py
@@ -1,1 +1,12 @@
-
+Class ListNode:
+def __init__(self, val=0, next=None);
+self.val=val
+self.next=next
+def remove_duplicates(head):
+  current = head
+  while current and current.next:
+    if current.val==current.next.val;
+    current.next=current.next.next
+else:
+current = current.next;
+return head;
